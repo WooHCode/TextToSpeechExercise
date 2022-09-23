9f4b611a353739b3f6ffe89f399eebde619a7808
WooHCode <gusdn526@naver.com>
2022-09-24 02:10:23 +0900

    Initial commit

---
+++ app/src/main/java/kr/tutorials/texttospeechexercise/MainActivity.kt
@@ -0,0 +1,57 @@
+package kr.tutorials.texttospeechexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import kr.tutorials.texttospeechexercise.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+    private var binding: ActivityMainBinding?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        tts = TextToSpeech(this,this)
+
+        binding?.btnStart?.setOnClickListener{
+            if(binding?.etText?.text!!.isEmpty()){
+                Toast.makeText(this, "텍스트를 입력해주세요",Toast.LENGTH_SHORT).show()
+            }else{
+                speakOut(binding?.etText?.text.toString())
+            }
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val result = tts!!.setLanguage(Locale.KOREA)
+
+            if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("TTS", "The Language specified is not supported!")
+            }else{
+                Log.e("TTS", "Initialization Failed!!")
+            }
+        }
+    }
+
+    private fun speakOut(text : String){
+        tts?.speak(text,TextToSpeech.QUEUE_FLUSH,null,"")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(tts!=null){
+            tts?.stop()
+            tts?.shutdown()
+        }
+        binding = null
+    }
+
+}
